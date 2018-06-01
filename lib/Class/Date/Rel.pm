25347eedfa9d8d827c5d7fc9090e48c807258207
Yanick Champoux <yanick@babyl.dyndns.org>
2018-06-01 09:07:00 -0400

    refactoring

--- lib/Class/Date/Rel.pm
+++ lib/Class/Date/Rel.pm
@@ -23,17 +23,18 @@ use overload
 sub new { my ($proto,$val)=@_;
   my $class = ref($proto) || $proto;
   return undef if !defined $val;
-  if (blessed($val) && $val->isa( __PACKAGE__ )) {
-    return $class->new_copy($val);
-  } elsif (ref($val) eq 'ARRAY') {
-    return $class->new_from_array($val);
-  } elsif (ref($val) eq 'HASH') {
-    return $class->new_from_hash($val);
-  } elsif (ref($val) eq 'SCALAR') {
-    return $class->new_from_scalar($$val);
-  } else {
-    return $class->new_from_scalar($val);
-  };
+
+  my $ref = ref $val or return $class->new_from_scalar($val);
+
+  return $class->new_copy($val)
+    if (blessed($val) && $val->isa( __PACKAGE__ ));
+    
+  return $class->new_from_array($val) if $ref eq 'ARRAY';
+
+  return $class->new_from_hash($val) if $ref eq 'HASH';
+
+  # can only be a scalar ref by now
+  return $class->new_from_scalar($$val);
 }
 
 sub new_copy { my ($s,$val)=@_;
